9f5a9c43e074e2e8b8b109bbfca705ab4df145c2
Swaroop Sethumadhavan <swaroop.striker@gmail.com>
2022-03-06 19:11:40 +0100

    Fix initial FOUC if dark theme is detected

--- assets/js/default.page.js
+++ assets/js/default.page.js
@@ -1,33 +1,51 @@
 const $themeToggleSwitch = document.getElementById("theme-switch");
 const $html = document.documentElement;
 
-function checkDarkMode() {
-  return (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  );
+function setThemePreference({ isDarkMode }) {
+  if (window.localStorage) {
+    window.localStorage.setItem('site__theme', isDarkMode ? 'dark' : 'light');
+  }
 }
 
-function switchToDarkMode(isDarkMode) {
+function getThemePreference() {
+    return window.localStorage && window.localStorage.getItem('site__theme');
+}
+
+function toggleDarkMode(isDarkMode) {
   if (isDarkMode) {
     $html.classList.add("mode-dark");
   } else {
     $html.classList.remove("mode-dark");
   }
-
-  $themeToggleSwitch.checked = isDarkMode;
 }
 
-function toggleDarkMode() {
-  const isDarkMode = $html.classList.contains("mode-dark");
+class ThemeSwitcher extends HTMLElement {
+  constructor() {
+    super();
 
-  switchToDarkMode(!isDarkMode);
+    this.$toggleSwitch = this.querySelector('#theme-switch')
+    this.$toggleSwitch.checked = this.checkedState;
+
+    this.$toggleSwitch.addEventListener('change', this.handleThemeChange);
+  }
+
+  get checkedState() {
+    const theme = getThemePreference();
+    const userPrefersDarkMode = window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (theme) {
+      return theme === 'dark';
+    }
+
+    return userPrefersDarkMode;
+  }
+
+  handleThemeChange(e) {
+    const isDarkMode = e.target.checked;
+    setThemePreference({ isDarkMode });
+    toggleDarkMode(isDarkMode);
+  }
 }
 
-if (checkDarkMode()) {
-  switchToDarkMode(true);
-} else {
-  switchToDarkMode(false);
-}
-
-$themeToggleSwitch.addEventListener("click", toggleDarkMode);
+customElements.define('theme-switcher', ThemeSwitcher);
