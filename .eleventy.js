17f462bb1ad4e488cc28ebea8e3e8853bdd28041
Swaroop Sethumadhavan <swaroop.striker@gmail.com>
2023-05-09 10:13:55 +0200

    Add passthrough for _redirects

--- .eleventy.js
+++ .eleventy.js
@@ -1,27 +1,28 @@
-const { Liquid } = require("liquidjs");
-const readingTime = require("reading-time");
+const { Liquid } = require('liquidjs');
+const readingTime = require('reading-time');
 
-module.exports = config => {
-  config.setLibrary("liquid", new Liquid({ extname: ".liquid" }));
+module.exports = (config) => {
+  config.setLibrary('liquid', new Liquid({ extname: '.liquid' }));
   config.setDataDeepMerge(true);
   config.addPassthroughCopy({
-    "./assets/images": "assets/images",
-    "./assets/cv.pdf": "assets/cv.pdf",
+    './assets/images': 'assets/images',
+    './assets/cv.pdf': 'assets/cv.pdf',
+    './_redirects': '_redirects',
   });
 
-  config.addFilter("readTime", text => readingTime(text).text);
-  config.addFilter("sanitizeurl", url =>
-    url === "/" ? url : url.replace(/\/*$/, "")
+  config.addFilter('readTime', (text) => readingTime(text).text);
+  config.addFilter('sanitizeurl', (url) =>
+    url === '/' ? url : url.replace(/\/*$/, '')
   );
 
   return {
     dir: {
       input: `./content`,
       output: `./_site`,
-      layouts: "../_layouts",
-      includes: "../_includes",
-      data: "../_data"
+      layouts: '../_layouts',
+      includes: '../_includes',
+      data: '../_data',
     },
-    htmlTemplateEngine: "liquid"
+    htmlTemplateEngine: 'liquid',
   };
 };
